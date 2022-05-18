61cd41ef135528629d42e397fdb60655c3cfb1a6
syawal07 <syawalaja0712@gmail.com>
2022-05-19 03:07:07 +0700

    remove and update files

--- js/main.js
+++ js/main.js
@@ -28,8 +28,9 @@ Vue.createApp({
         nama: "Syawal Saputra",
         role: "Pemula | Mahasiswa Universitas Ahmad Dahlan",
       },
+    
       about: {
-        hero: "./bahan/download.png",
+        hero: "./bahan/Sywl.png",
         tentang: "Tentang",
         penulis: "ditulis oleh",
         a: {
@@ -38,11 +39,11 @@ Vue.createApp({
           tgl: ". 07 Desember 2002 .",
         },
         deskripsi:
-          "Perkenalkan nama saya Syawal Saputra biasa di panggil syawal. Saya adalah anak pertama dengan kelahiran 2002 di Pangkep, Sulawesi Selatan.",
-        deskripsi2:
-          "Saya terlahir sebagai keluarga yang sederhana. Dan saya sangat bersyukur bisa mendapat beasiswa kader muhammadiyah sehingga saya diberi kesempatan untuk kuliah di Universitas Ahmad Dahlan Jogjakarta. Dan disini kisah perjalan saya diluar daerah dimulai.",
-        deskripsi3:
-          "Organisasi didalam lingkup muhammadiyah yang saya ikuti telah merubah diriku. Dimulai dari masalah ibadah, retorika dalam berbicara dan akademik yang tersusun dengan baik sehingga ini yang menjadi salah satu alasan mengapa aku diberi kesempatan kuliah disini.",
+          "Perkenalkan nama saya Syawal Saputra biasa di panggil syawal. Saya adalah anak pertama dengan kelahiran 2002 di Pangkep, Sulawesi Selatan. Saya terlahir sebagai keluarga yang sederhana.Dan saya sangat bersyukur bisa mendapat beasiswa kader muhammadiyah sehingga saya diberi kesempatan untuk kuliah di Universitas Ahmad Dahlan Jogjakarta. Dan disini kisah perjalan saya diluar daerah dimulai.  Organisasi didalam lingkup muhammadiyah yang saya ikuti telah merubah diriku. Dimulai dari masalah ibadah, retorika dalam berbicara dan akademik yang tersusun dengan baik sehingga ini yang menjadi salah satu alasan mengapa aku diberi kesempatan kuliah disini.",
+
+          
+        
+         
 
         pendidikan: "Pendidikan",
         Sekolah: {
@@ -66,50 +67,7 @@ Vue.createApp({
           alamat2: "Jl. Ringroad Selatan, Kragilan, Tamanan, Kec. Banguntapan, Kabupaten Bantul, Daerah Istimewa Yogyakarta 55191",
           link2: "https://uad.ac.id/id/",
         },
-        hardskill: "Hard Skill",
-        hard: {
-          figma: "Figma",
-          persentase: "50%",
-          deskfigma:
-            "Saya lumayan tertarik dibidang desain terutama UI/UX desain, dengan begitu saya mempelajari salah satu framework bernama figma dan sudah menyelesaikan beberapa projek prototype dengan menggunakan figma.",
-          tentangfigma: "Figma ...",
-          urlfigma: "https://www.figma.com/",
-          projekfigma: "Riwayat projek",
-          urlprojek: "https://www.figma.com/file/pe4nJOvBQgBjTytAOi0orw/ZONE?node-id=0%3A1",
 
-          html: "HTML",
-          persentasehtml: "60%",
-          deskhtml:
-            "Saya juga masih saat ini sedang mempelajari front end development sehingga saya mempelajari HTML atau yang biasa di sebut Hypertext Markup Language, namu saya masih perlu belajar lebih giat lagi untuk mengembangkan ini. Bahasa markup yang digunakan untuk membuat sebuah halaman web. ",
-          tentanghtml: "HTML ...",
-          urlhtml: "https://www.w3schools.com/html/default.asp",
-          projekhtml: "Riwayat projek",
-          urlprojek2: "https://github.com/syawal07/Tekweb2022",
-
-          css: "CSS",
-          persentasecss: "55%",
-          deskcss:
-            "Saya juga sedang menekuni front end development sehingga saya mempelajari CSS atau yang biasa di sebut Cascading Style Sheet. CSS bekerja saat sebuah browser menampilkan dokumen, sebabnya pada waktu itu browser harus menggabungkan konten dokumen dengan gaya informasi atau gaya presentasi dalam halaman sebuah situs.",
-          tentangcss: "CSS ...",
-          urlcss: "https://www.w3schools.com/css/",
-          projekcss: "Riwayat projek",
-          urlprojek3: "https://github.com/syawal07/Tekweb2022/tree/master/css",
-
-          js: "Javascript",
-          persentasejs: "40%",
-          deskjs:
-            "Dan terakhir saya juga masih mempelajari java script karena mempelajari JavaScript membuat pengembangan website agar lebih dinamis dan interaktif.",
-          tentangjs: "Javascript ...",
-          urljs: "https://www.w3schools.com/js/default.asp",
-          projekjs: "Riwayat projek",
-          urlprojek4: "https://github.com/syawal07/Tekweb2022/tree/master/js",
-        },
-        softskill: {
-          judul: "Hobby",
-          ps: "Membaca",
-          psv: "Olahraga",
-          ct: "Berimajinasi",
-        },
       },
       contact: {
         judul: "Contact Me",
@@ -149,3 +107,19 @@ Vue.createApp({
     this.getHeaderData(); //eksekusi fungsi getHeaderData() pada bagian methods saat halaman terbuka
   },
 }).mount("#app");
+
+window.onload = function () {
+  var elements = document.getElementsByClassName('txt-rotate');
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-rotate');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtRotate(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement('style');
+  css.type = 'text/css';
+  css.innerHTML = '.txt-rotate > .wrap { border-right: 0.08em solid #666 }';
+  document.body.appendChild(css);
+};
